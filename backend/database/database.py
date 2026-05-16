a1961a02e61f769c9fece974d435fc16b6b4dcce
Thirubts2005 <161499141+Thirubts2005@users.noreply.github.com>
2026-05-16 13:15:15 +0530

    Update database.py

--- backend/database/database.py
+++ backend/database/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Example: postgresql://username:password@localhost:5432/jobs_db
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Thirumalaib!2k5@localhost:5432/postgres")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://username:password@localhost:5432/jobs_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
